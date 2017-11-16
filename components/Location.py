b9a9b660adf5570f7f7fd7e1d9fd8fd72a912e9a
Akhila Sankar <sankar.a@husky.neu.edu>
2017-11-15 22:56:38 -0800

    Minor refactoring

--- components/Location.py
+++ components/Location.py
@@ -16,11 +16,17 @@ def list_cities(cities):
 
 def getWeather(city):
 	cityName = city.split(", ")[0]
+	#Get details of city with name
+	
 	response = requests.get("https://www.metaweather.com/api/location/search?query="+ cityName)
 	responseJson = response.json()
+	
 	if (len(responseJson) > 0):
+		#Get the where on earth ID
 		woeid = responseJson[0]["woeid"]
-		weatherData = requests.get("https://www.metaweather.com/api/location/"+str(woeid)).json()
+		
+		#Get weatherData based on woeid
+		weatherData = requests.get("https://www.metaweather.com/api/location/" + str(woeid)).json()
 		weather = weatherData["consolidated_weather"][0]["weather_state_name"]
 		return weather
 	else:
